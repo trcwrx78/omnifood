303ed3931ac531392c579a8386c3a6798438d29d
Torrence Cole <trcole@gmail.com>
2020-07-07 11:02:38 -0400

    Added php file for reference

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  // For the sticky navigation
   $('.js--section-features').waypoint(
     function (direction) {
       if (direction == 'down') {
@@ -12,9 +13,93 @@ $(document).ready(function () {
     }
   );
 
-  // var waypoints = $('#handler-first').waypoint(function(direction) {
-  //     notify(this.element.id + ' hit 25% from top of window')
-  //   }, {
-  //     offset: '25%'
-  //   })
+  // Scroll on buttons
+  $('.js--scroll-to-plans').click(function () {
+    $('html, body').animate(
+      { scrollTop: $('.js--section-plans').offset().top },
+      1000
+    );
+  });
+
+  $('.js--scroll-to-start').click(function () {
+    $('html, body').animate(
+      { scrollTop: $('.js--section-features').offset().top },
+      1000
+    );
+  });
+
+  // Navigation scroll
+  $(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+      if (
+        location.pathname.replace(/^\//, '') ==
+          this.pathname.replace(/^\//, '') &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate(
+            {
+              scrollTop: target.offset().top,
+            },
+            1000
+          );
+          return false;
+        }
+      }
+    });
+  });
+
+  // Animations on scroll
+  $('.js--wp-1').waypoint(
+    function (direction) {
+      $('.js--wp-1').addClass('animated fadeIn');
+    },
+    {
+      offset: '50%',
+    }
+  );
+
+  $('.js--wp-2').waypoint(
+    function (direction) {
+      $('.js--wp-2').addClass('animated fadeInUp');
+    },
+    {
+      offset: '50%',
+    }
+  );
+
+  $('.js--wp-3').waypoint(
+    function (direction) {
+      $('.js--wp-3').addClass('animated fadeIn');
+    },
+    {
+      offset: '50%',
+    }
+  );
+
+  $('.js--wp-4').waypoint(
+    function (direction) {
+      $('.js--wp-4').addClass('animated pulse');
+    },
+    {
+      offset: '50%',
+    }
+  );
+
+  // Mobile nav
+  $('.js--nav-icon').click(function () {
+    var nav = $('.js--main-nav');
+    var icon = $('.nav-icon');
+
+    nav.slideToggle(200);
+    if (icon.attr('name') == 'add-outline') {
+      icon.attr('name', 'add-circle-outline');
+    } else {
+      icon.attr('name', 'add-outline');
+    }
+  });
 });
